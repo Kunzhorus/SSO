2008ab78ccb8b1ce4796bf724eef8ae5dd72f0f9
Kunzhorus <tranbuinhukhanh@gmail.com>
2023-10-02 19:37:33 +0700

    commit

--- Frontend/src/components/GroupRole/GroupRole.js
+++ Frontend/src/components/GroupRole/GroupRole.js
@@ -85,7 +85,7 @@ function GroupRole() {
     return result
   };
 
-  
+
   const handleSaveAssign = async () => {
     let data = buildDataToSave();
     let response = await assignRoleToGroup(data);
@@ -93,7 +93,6 @@ function GroupRole() {
       toast.success(response.EM)
     }
   };
-
 
   return (
     <div className="group-role-cotainer container">
